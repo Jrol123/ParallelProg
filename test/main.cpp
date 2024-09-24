8ad3364c4ef2a3d260cceadff8857c3fcee54105
Artemii <angap4@gmail.com>
2024-09-24 22:39:35 +1000

    Initial commit

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+
+
+// C++ program to illustrate the use
+// of cout object
+#include <iostream>
+using namespace std;
+ 
+// Driver Code
+int main()
+{
+    cout << "Welcome to GFG";
+ 
+    return 0;
+}
